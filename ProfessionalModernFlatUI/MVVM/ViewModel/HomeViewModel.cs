ca4c8eaa6ead918a4ecd0d3a79f1e03c21254dae
shahmeer <shahmeer@esparkconsultants.com>
2022-11-22 16:12:01 +0500

    Add project files.

---
+++ ProfessionalModernFlatUI/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalModernFlatUI.MVVM.ViewModel
+{
+    class HomeViewModel
+    {
+    }
+}
---
+++ ProfessionalModernFlatUI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ProfessionalModernFlatUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessionalModernFlatUI.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoverViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoverViewModel DisocverVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DisocverVM = new DiscoverViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            DiscoverViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DisocverVM;
+            });
+        }
+
+    }
+}
